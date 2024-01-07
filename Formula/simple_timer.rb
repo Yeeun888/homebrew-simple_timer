a3cbd43b7cc22d6b84ff3cf88eb5249334d2505f
Yeeun888 <108403458+Yeeun888@users.noreply.github.com>
2024-01-08 00:10:54 +0700

    Fixed Formula

--- Formula/simple_timer.rb
+++ Formula/simple_timer.rb
@@ -1,9 +1,9 @@
 class simple_timer < Formula
   desc "Easy to use CLI clock/stopwatch"
   homepage "https://github.com/Yeeun888/simple_timer"
-  url "https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz"
-  sha256 "c0902320a4ff22f91e54797db290ac18bf2b765732842ddf66d8df8c4fe1176c"
-  version "0.0.2"
+  url "https://github.com/Yeeun888/simple_timer/releases/download/v0.1.0/simpletimer.tar.gz"
+  sha256 "f457c02cf0bb3200b33602ef27413ff6cdcee9ccbcd34c64a0995d835f03ca5a"
+  version "v0.1.0"
 
   def install
     bin.install "simpletimer"
